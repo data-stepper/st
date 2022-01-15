851da703255832670d25a3232db4f6a392f54aa6
Max Schillinger <maxschillinger@web.de>
2022-01-15 13:46:15 +0100

    apply st-alpha-0.8.2.diff

--- config.def.h
+++ config.def.h
@@ -94,6 +94,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.95;
+
 /* Terminal colors (16 first used in escape sequence) */
 /* Alacritty's default colors */
 static const char *colorname[] = {
